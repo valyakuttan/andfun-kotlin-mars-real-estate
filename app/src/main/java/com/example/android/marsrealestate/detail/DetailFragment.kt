a652bae21336cbf51b8e54a9e92abe608a85e766
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-28 23:58:15 +0530

    Step 07 completed

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 
 /**
@@ -29,13 +30,23 @@ import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 class DetailFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        @Suppress("UNUSED_VARIABLE")
+
         val application = requireNotNull(activity).application
+
         val binding = FragmentDetailBinding.inflate(inflater)
+
         binding.setLifecycleOwner(this)
+
         // TODO (14) Get the selectedProperty from the fragment arguments with DetailFragmentArgs
+        val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+
         // TODO (15) Create the DetailViewModelFactory using the marsProperty and application
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+
         // TODO (16) Get the DetailViewModel from the DetailViewModelFactory and set it in the binding
+        binding.viewModel = ViewModelProviders.of(
+                this, viewModelFactory).get(DetailViewModel::class.java)
+
         return binding.root
     }
 }
