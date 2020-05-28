01b0eda559322642b4d0c671d2f65a593c86c64e
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-28 12:26:52 +0530

    Use coroutines and deferred jobs with exception handling to retirive resources from network

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,9 +17,10 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -41,6 +42,7 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         // TODO (02) Use .addCallAdapterFactory to add the CoroutineCallAdapterFactory
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(BASE_URL)
         .build()
 
@@ -56,7 +58,7 @@ interface MarsApiService {
     @GET("realestate")
     fun getProperties():
     // TODO (03) Change the return type from our getProperties call to Deferred
-            Call<List<MarsProperty>>
+            Deferred<List<MarsProperty>>
 }
 
 /**
--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,10 +21,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
-import com.example.android.marsrealestate.network.MarsProperty
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -40,6 +40,10 @@ class OverviewViewModel : ViewModel() {
 
 
     // TODO (04) Create a coroutine Job and a CoroutineScope using the Main Dispatcher
+    private var viewModelJob = Job()
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
+
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -52,19 +56,27 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the response LiveData to the Mars API status or the successful number of
      * Mars properties retrieved.
      */
+
     private fun getMarsRealEstateProperties() {
         // TODO (05) Call coroutineScope.launch and place the rest of the code in it
-        // TODO (06) Call MarsApi.retrofitService.getProperties and call await on the Deferred
-        // TODO (07) Surround the Retrofit code with a try/catch, and set _response appropriately
-        MarsApi.retrofitService.getProperties().enqueue( object: Callback<List<MarsProperty>> {
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure: " + t.message
+        coroutineScope.launch {
+
+            // TODO (06) Call MarsApi.retrofitService.getProperties and call await on the Deferred
+            var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+
+            // TODO (07) Surround the Retrofit code with a try/catch, and set _response appropriately
+            try {
+                var listResult = getPropertiesDeferred.await()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
             }
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
-            }
-        })
+        }
     }
 
     // TODO (08) Cancel the Coroutine Job when the ViewModel is finished in onCleared
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
